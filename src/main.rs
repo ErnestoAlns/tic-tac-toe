3415c86712f1b29ee0c742ccd3b7079883d7ffd9
Ernesto9139 <ernesto9139@icloud.com>
2025-11-21 01:58:25 -0600

    feat: game board

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,47 @@
-struct Matriz<T> {
-    data: [[T; 3]; 3],
+use colored::*;
+
+struct Matrix {
+    boxes: Vec<char>
 }
 
-impl Matriz<i32> {
-    pub fn identidad() -> Self {
-        Matriz {
-            data: [
-                [1, 0, 0],
-                [0, 1, 0],
-                [0, 0, 1],
-            ],
-        }
-    }
-
-    pub fn imprimir(&self) {
-        println!("Matriz Identidad 3x3:");
-        for fila in &self.data {
-            for elemento in fila {
-                print!("{: <2}", elemento);
+impl Matrix {
+    fn print_vec(self) {
+        for (i, item) in self.boxes.iter().enumerate() {
+            if i % 3 != 0 {
+                print!("{}", "|".blue());
             }
-            println!();
+            print!(" {} ", item);
+            if (i+1) % 3 == 0 && i != 8 {
+                println!();
+                println!("{}","---+---+---".blue()); 
+            }
         }
+    }  
+
+    fn play(&mut self, position: usize, player: char) {
+        self.boxes[position] = player
     }
 }
 
 fn main() {
-    let mi_matriz = Matriz::identidad();
-    mi_matriz.imprimir();
+    start_game();
+
+    let mut game_board = Matrix {
+        boxes: ('1'..='9').collect()
+    };
+    let player:char = 'X';
+    let position:usize = 2;
+
+//    Matrix::play(&mut gameboard, position, player);
+    Matrix::print_vec(game_board);
+    end_game();
+}
+fn start_game() {
+    println!("{}","========= TIC TAC TOE =========".red());
 }
 
+fn end_game() {
+    println!();
+    println!();
+    println!("{}", "==============================".red());
+}
